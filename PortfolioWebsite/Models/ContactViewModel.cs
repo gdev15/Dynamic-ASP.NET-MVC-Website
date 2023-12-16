fdc5333044ff92902da2ec833213dbeacfb31ee9
Gabriel Ramirez <gramirez@webgdesigns.com>
2023-12-16 09:08:41 -0600

    Updated

--- PortfolioWebsite/Models/ContactViewModel.cs
+++ PortfolioWebsite/Models/ContactViewModel.cs
@@ -6,11 +6,6 @@ namespace PortfolioWebsite.Models
     [Keyless] //There is no primary key cause there is no DB table so explicitly state it
     public class ContactViewModel
     {
-        //We can use Data Annotation to Add validation to our model.
-        //This is useful when we have required fields or need certain kinds of information.
-
-        //? means property is nullable if we want to get rid of the green lines
-        //public string Name? {get; set;}
 
         [Required(ErrorMessage = "*Name is required*")]
         public string Name { get; set; }
@@ -25,11 +20,7 @@ namespace PortfolioWebsite.Models
         [Required(ErrorMessage = "*Message is required*")]
         [DataType(DataType.MultilineText)] //MultilineText denotes this field is larger than a standard textbox (<input> => <textarea>)
         public string Message { get; set; }
-
-        //MINI-LAB!
-        //Create the ContactViewModel in your PersonalSite MVC Solution.
-        //You can copy & paste teh properties & data annotations here
-        //into that solution. Don't forget the using statement annotations
+  
     }
 }
 
